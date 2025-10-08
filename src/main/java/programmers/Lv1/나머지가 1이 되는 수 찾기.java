ef03fe629afd7739f9523a062b3c38cc428d5032
OneTwo <102886829+kim-hani@users.noreply.github.com>
2025-10-08 15:22:34 +0900

    Create 나머지가 1이 되는 수 찾기.java

---
+++ src/main/java/programmers/Lv1/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = n-1;
+        for(int i =2 ; i<n/2;i++){
+            if(n%i == 1){
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
